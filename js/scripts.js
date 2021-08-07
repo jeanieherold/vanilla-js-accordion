5c2e63985fa9538ebb1ff932ae179e84c55a1112
Jeanie Herold <jeaniemherold68@gmail.com>
2021-08-07 12:40:37 -0400

    slide closer but not quite yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,33 @@
 
-function initAcc(elem, option){
-    document.addEventListener('click', function (e) {
-        if (!e.target.matches(elem+' .accordion-btn')) return;
-        else{
-            if(!e.target.parentElement.classList.contains('active')){
-                if(option==true){
-                    var elementList = document.querySelectorAll(elem+' .a-container');
-                    Array.prototype.forEach.call(elementList, function (e) {
-                        e.classList.remove('active');
-                    });
-                }            
-                e.target.parentElement.classList.add('active');
-            }else{
-                e.target.parentElement.classList.remove('active');
+const accordBtns = document.querySelectorAll('.accordion-btn');
+
+accordBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        const siblings = getSiblings(button.parentElement);
+        siblings.forEach((sibling) => {
+            if (sibling.classList.contains('active')) {
+                sibling.classList.remove('active');
             }
-        }
+        })
+        button.parentElement.classList.toggle('active');      
     });
-}
+});
 
-initAcc('.accordion', true);
+//find all siblings of an element
+var getSiblings = function (elem) {
 
+	// Setup siblings array and get the first sibling
+	var siblings = [];
+	var sibling = elem.parentNode.firstChild;
+
+	// Loop through each sibling and push to the array
+	while (sibling) {
+		if (sibling.nodeType === 1 && sibling !== elem) {
+			siblings.push(sibling);
+		}
+		sibling = sibling.nextSibling
+	}
+
+	return siblings;
+
+};
